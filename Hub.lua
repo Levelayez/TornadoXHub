740c54e45481b3a73680ac4691d066f9c0655eda
Levelayez <67072667+Levelayez@users.noreply.github.com>
2021-09-14 16:26:02 -0400

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Tornado X", "Synapse")
+local Window = Library.CreateLib("Tornado X", "Sentinal")
 local Tab = Window:NewTab("Main")
 local Tab = Window:NewTab("Player")
 local PSection = Tab:NewSection("Player")
